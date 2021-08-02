8d7e92bc196ae2ba9014b013470e9c413e14f75e
sayandey-dev <sayandeyip@gmail.com>
2021-08-02 12:28:28 +0530

    Updated

--- Issuebook.java
+++ Issuebook.java
@@ -10,7 +10,7 @@ public class Issuebook {
         JFrame issuebook = new JFrame("Issue Book");
         issuebook.setLayout(null);
         issuebook.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        issuebook.setSize(800, 600);
+        issuebook.setSize(400, 500);
 
         JLabel bookid = new JLabel();
         bookid.setText("Book id: ");
@@ -27,9 +27,40 @@ public class Issuebook {
         issuebook.add(bookname);
 
         JTextField booknamefield = new JTextField();
-        booknamefield.setBounds(100, 84, 100, 30);
+        booknamefield.setBounds(100, 84, 150, 30);
         issuebook.add(booknamefield);
 
+        JLabel Customer_id = new JLabel();
+        Customer_id.setText("Customer id: ");
+        Customer_id.setBounds(20, 130, 80, 40);
+        issuebook.add(Customer_id);
+
+        JTextField cid = new JTextField();
+        cid.setBounds(110, 134, 100, 30);
+        issuebook.add(cid);
+
+        JLabel customer_name = new JLabel();
+        customer_name.setText("Customer name: ");
+        customer_name.setBounds(20, 180, 110, 40);
+        issuebook.add(customer_name);
+
+        JTextField cname = new JTextField();
+        cname.setBounds(120, 184, 150, 30);
+        issuebook.add(cname);
+
+        JLabel customer_phone = new JLabel();
+        customer_phone.setText("Customer phone: ");
+        customer_phone.setBounds(20, 230, 120, 40);
+        issuebook.add(customer_phone);
+
+        JTextField cphone = new JTextField();
+        cphone.setBounds(120, 234, 140, 30);
+        issuebook.add(cphone);
+
+        JButton issue = new JButton();
+        issue.setText("Issue book");
+        issue.setBounds(80, 300, 130, 50);
+        issuebook.add(issue);
 
         issuebook.setVisible(true);
     }
--- Operations.java
+++ Operations.java
@@ -14,22 +14,22 @@ public class Operations {
 
         JButton booklist = new JButton();
         booklist.setText("Book List");
-        booklist.setBounds(120, 40, 90, 50);
+        booklist.setBounds(120, 40, 130, 50);
         operations.add(booklist);  
 
         JButton addbook = new JButton();
         addbook.setText("Add book");
-        addbook.setBounds(120, 150, 90, 50);
+        addbook.setBounds(120, 150, 130, 50);
         operations.add(addbook);
 
         JButton issuebook = new JButton();
         issuebook.setText("Issue book");
-        issuebook.setBounds(380, 40, 90, 50);
+        issuebook.setBounds(380, 40, 130, 50);
         operations.add(issuebook);
 
         JButton returnbook = new JButton();
         returnbook.setText("Return book");
-        returnbook.setBounds(380, 150, 90, 50);
+        returnbook.setBounds(380, 150, 130, 50);
         operations.add(returnbook);
 
 
